342f808344219393d3acb6b6f8af8d432c2e48ec
auroja2 <126101729+auroja2@users.noreply.github.com>
2025-02-22 23:27:21 +0530

    Delete code.py

--- code.py
+++
@@ -1,8 +0,0 @@
-import os
-
-scaler_path = os.path.join("./Models", "scaler.pkl")
-model_path = os.path.join("./Models", "model.pkl")
-
-print("Scaler Path:", scaler_path)
-print("Model Path:", model_path)
-
